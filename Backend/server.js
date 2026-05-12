1e04b214a19dd8569c999b7e73eb7df44cae830a
Shubhayu Barua <youremail@example.com>
2026-05-12 22:21:33 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,10 @@ const app = require("./src/app");
 const connectDb = require("./src/db/db");
 const server = require("./map_server")
 
- 
+ const PORT = process.env.PORT || 3005;
 require("dotenv").config();
 connectDb();
   
-server.listen(process.env.PORT || 3005,"0.0.0.0", () => {
-  console.log(`Server running on ${process.env.PORT || 3005}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server is live on port ${PORT}`);
 });
